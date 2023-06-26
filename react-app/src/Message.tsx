360701ed774c1a65f49dce1ac90ca35924071be8
Logangosha <Logangosha@gmail.com>
2023-06-26 18:45:33 -0500

    Finished account page

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,11 @@
+//PascalCasing
+function Message(){
+    //jsx: javascript xml
+    const name = "Logan";
+    if(name)
+    return <h1>Hello {name}</h1>;
+    else
+    return <h1>Hello World</h1>;
+}
+
+export default Message;
